760c25873ff04aabfe54635b7c2ccd8880b6fb12
Samir Bravo <sambravo@gmail.com>
2025-01-22 17:35:29 +0000

    Arrays chalenge solution

--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,17 @@
 def find_second_smallest(my_list):
-    return 0
+    smallest = my_list[0]
+    second_smallest = my_list[0]
+    for i in my_list:
+        if (i < smallest) :
+            second_smallest = smallest
+            smallest = i
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+        elif(i < second_smallest and i != smallest) :
+            second_smallest = i
+    return second_smallest
+
+def find_second_smallest_v2(my_list):
+    sorted_list = sorted(my_list)
+    return sorted_list[1]
+
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
